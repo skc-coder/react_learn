d809b91ca43fce75c32f11f402781e1e6be6f7a1
84 <84ya+github@proton.me>
2026-05-10 16:39:54 +0530

    header done

---
+++ react_project/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header/Header.jsx";
+// import Footer from "./components/Footer/Footer.jsx";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default App;
